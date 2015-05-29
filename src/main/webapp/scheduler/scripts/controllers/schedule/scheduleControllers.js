750f436e1e6e95edc12615ee7d2e3b642ca8de9e
Marian Spoiala <Marian.Spoiala@teamnet.ro>
2015-05-29 17:43:24 +0300

    Update - edit works with custom fired intervals

--- src/main/webapp/scheduler/scripts/controllers/schedule/scheduleControllers.js
+++ src/main/webapp/scheduler/scripts/controllers/schedule/scheduleControllers.js
@@ -1,183 +1,11 @@
 'use strict';
-/*
- schedulerControllers
- .controller('ScheduleController', function ($scope,AppGridConstants, Schedule, TimeInterval, Month, DayOfWeek, RecurrentTimeUnit, ScheduledJob) {
-
-
- $scope.timeIntervals = TimeInterval.query();
- $scope.months = Month.query();
-
-
- $scope.dayOfWeeks = DayOfWeek.query();
- $scope.recurrentTimeUnits = RecurrentTimeUnit.query();
- $scope.scheduledJobs = ScheduledJob.query();
-
- $scope.showTimeInterval = false;
- $scope.showMonth = false;
- $scope.showDayOfWeek = false;
- $scope.showRecurrentTimeUnit = false;
- $scope.showScheduledJob = false;
- $scope.actionEvent="scheduleGrid";
- $scope.showEditBtn = true;
- $scope.showCreateOrEdit = false;
- $scope.showBtns = false;
- $scope.showDetails = false;
- $scope.showGrid = true;
-
- $scope.selected = function(){};
-
- $scope.$root.$on('scheduleGridSelection',function(data){
- $scope.showBtns = true;
- });
-
- $scope.redrawGrid = function(grid) {
-
- $scope.searchTerms=[];
- $scope.searchTerms.push(
- {
- property: 'schedule.id',
- value: parseInt($scope.selected()[0].id),
- type: AppGridConstants.searchFilterTypes.EQUAL,
- negation: false
- }
- );
-
- if(grid == 'timeInterval'){
- $scope.showTimeInterval = true;
-
-        $scope.appGrid = {
-            url:'app/rest/schedule/list',
-            id : 'schedule'
-        };
-
- $scope.showTimeInterval = false;
- $scope.showDayOfWeek = false;
- $scope.showRecurrentTimeUnit = false;
- $scope.showScheduledJob = false;
- }
- if(grid == 'dayOfWeek'){
- $scope.showDayOfWeek = true;
-
- $scope.showTimeInterval = false;
- $scope.showMonth = false;
- $scope.showRecurrentTimeUnit = false;
- $scope.showScheduledJob = false;
- }
- if(grid == 'recurrentTimeUnit'){
- $scope.showRecurrentTimeUnit = true;
-
- $scope.showTimeInterval = false;
- $scope.showMonth = false;
- $scope.showDayOfWeek = false;
- $scope.showScheduledJob = false;
- }
- if(grid == 'scheduledJob'){
- $scope.showScheduledJob = true;
-
- $scope.showTimeInterval = false;
- $scope.showMonth = false;
- $scope.showDayOfWeek = false;
- $scope.showRecurrentTimeUnit = false;
- }
- };
-
- $scope.createOrUpdate = function () {
- $scope.showGrid = true;
- Schedule.save($scope.schedule,
- function () {
- $scope.showCreateOrEdit = false;
- $scope.refresh();
- });
- };
-
- $scope.clear = function () {
- $scope.schedule = {active: null, recurrent: null, cron: null, startTime: null, endTime: null, repetitions: null, id: null};
- $scope.searchTerms = [];
- $scope.showDetails=false;
- };
-
- $scope.showDelete = function (id) {
- if(!_.isEmpty($scope.selected())){
- $('#deleteScheduleConfirmation').modal('show');
- }
- };
-
- $scope.confirmDelete = function (id) {
- Schedule.delete({id: $scope.selected()[0].id},
- function () {
- $scope.refresh();
- $('#deleteScheduleConfirmation').modal('hide');
- });
- };
-
- $scope.refreshEvent = "refreshEvent";
- $scope.selected;
- $scope.refreshFunction;
-
- $scope.appGrid = {
- url:'app/rest/schedule/list',
- id : 'schedule',
- title :  'Lista schedule'
- };
-
- $scope.functionality='Schedule';
-
- $scope.customOptions = {
- enableRowSelection: true,
- enableRowHeaderSelection: false,
- multiSelect: false,
- noUnselect: true,
- enableSorting: true,
- rowTemplate: "<div ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>"
- };
-
-
- $scope.refresh = function(){
- $scope.$broadcast($scope.refreshEvent);
-
-
- };
-
- $scope.back = function(){
- $scope.showCreateOrEdit = false;
- $scope.searchTerms=[];
- $scope.showGrid = true;
- };
-
- $scope.showAdd = function(){
- $scope.clear();
- $scope.showCreateOrEdit = true;
- $scope.showGrid = false;
- if ($scope.timeInterval != null) {
- $scope.schedule.timeInterval = $scope.timeInterval;
- }
- if ($scope.scheduledJob != null) {
- $scope.schedule.scheduledJob = $scope.scheduledJob;
- }
- };
-
- $scope.showEdit = function(){
- if((!_.isEmpty($scope.selected())) ){
- var id = parseInt($scope.selected()[0].id);
- Schedule.get({id: id}, function(data){
- $scope.schedule = data;
- });
- $scope.showCreateOrEdit = true;
- $scope.showDetails = true;
- $scope.showGrid = false;
- }
- };
-
-
- });
- */
 
 schedulerControllers
     .controller('ScheduleController', function ($scope, AppGridConstants, Schedule, TimeInterval, RecurrentTimeUnit, ScheduledJob, CustomFireTimes, TimeUnit) {
 
         $scope.timeUnits = TimeUnit.query();
         $scope.timeIntervals = TimeInterval.query();
-        $scope.recurrentTimeUnits = RecurrentTimeUnit.query();
+        //$scope.recurrentTimeUnits = RecurrentTimeUnit.query();
         $scope.scheduledJobs = ScheduledJob.query();
 
         $scope.showTimeInterval = false;
@@ -215,7 +43,7 @@ schedulerControllers
                 else if ($scope.selectedRepetitionType == $scope.repetitionTypes[1]) {
                     $scope.regularIntervals = false;
                     $scope.schedule.timeInterval = null;
-                    resetCustomFiresIntervals();
+                    setCustomFiresFromCTU();
                 }
             }
 
@@ -290,6 +118,7 @@ schedulerControllers
                     console.log(data);
                 });
         }
+
         function resetCustomFiresIntervals() {
             /* Resetare luni */
             $scope.allMonthsButton.pushed = false;
@@ -318,7 +147,34 @@ schedulerControllers
             $scope.nrSelectedCustom = 0;
         }
 
+        function setCustomFiresFromCTU() {
+            resetCustomFiresIntervals();
+            if ($scope.schedule != null && $scope.schedule.recurrentTimeUnits != null) {
+                for (var i = 0; i < $scope.schedule.recurrentTimeUnits.length; i++) {
+                    console.log($scope.schedule.recurrentTimeUnits[i].timeUnit.code);
+                    if ($scope.schedule.recurrentTimeUnits[i].timeUnit.code == "MON") {
+                        $scope.monthsButtons[$scope.schedule.recurrentTimeUnits[i].value-1].pushed = true;
+                        $scope.nrSelectedCustom++;
+                    }
+                    else if ($scope.schedule.recurrentTimeUnits[i].timeUnit.code == "D") {
+                        $scope.monthDaysButtons[$scope.schedule.recurrentTimeUnits[i].value-1].pushed = true;
+                        $scope.nrSelectedCustom++;
+                    }
+                    else if ($scope.schedule.recurrentTimeUnits[i].timeUnit.code == "W") {
+                        $scope.weekDaysButtons[$scope.schedule.recurrentTimeUnits[i].value-1].pushed = true;
+                        $scope.nrSelectedCustom++;
+                    }
+                    else if ($scope.schedule.recurrentTimeUnits[i].timeUnit.code == "H") {
+                        $scope.hoursButtons[$scope.schedule.recurrentTimeUnits[i].value-1].pushed = true;
+                        $scope.nrSelectedCustom++;
+                    }
+                }
+            }
+        }
+
         createCustomFiresIntervals();
+
+        setCustomFiresFromCTU();
 
         /* Numar cate butoane au fost apasate */
         function countSelected(pushed) {
@@ -496,6 +352,10 @@ schedulerControllers
             return null;
         }
 
+        function saveInRecTimeUnitsObj(obj) {
+
+        }
+
         /* Construire date pentru custom fire intervals */
         function buildRecurrentTimeUnits() {
             var recurrentTimeUnits = [];
@@ -507,7 +367,6 @@ schedulerControllers
                         var obj = {};
                         obj.value = $scope.monthsButtons[i].id;
                         obj.timeUnit = getTimeUnitIdByCode("MON");
-                        obj.schedule = {};
                         recurrentTimeUnits.push(obj);
                     }
                 }
@@ -598,6 +457,8 @@ schedulerControllers
         /* -------------------------------------------------------------------- */
 
         $scope.createOrUpdate = function () {
+            $scope.schedule.recurrentTimeUnits = {};
+            console.log($scope.schedule.startTime);
             $scope.showGrid = true;
 
             /* Daca este pe custom fire intervals, contruiesc arrayul */
@@ -679,10 +540,23 @@ schedulerControllers
                 var id = parseInt($scope.selected()[0].id);
                 Schedule.get({id: id}, function (data) {
                     $scope.schedule = data;
+
+                    //Daca este CUSTOM FIRED (adica time_interval == null, dar recurrent)
+                    if ($scope.schedule.recurrent == true && $scope.schedule.timeInterval == null) {
+                        $scope.selectedRepetitionType = $scope.repetitionTypes[1];
+                        $scope.regularIntervals = false;
+                        setCustomFiresFromCTU();
+                    }
+                    //Daca este REGULAR INTERVAL (adica recurrent == true, dar time_interval != null)
+                    else if ($scope.schedule.recurrent == true) {
+                        $scope.selectedRepetitionType = $scope.repetitionTypes[0];
+                    }
                 });
                 $scope.showCreateOrEdit = true;
                 $scope.showDetails = true;
                 $scope.showGrid = false;
+
+
             }
         };
 
